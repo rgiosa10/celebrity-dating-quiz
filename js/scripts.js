85eaee59d6e054a714eb950a35ff79dc3714f036
Kirsten Opstad <kirsten.opstad@gmail.com>
2022-10-19 17:05:59 -0700

    Add eventListener functionality to replace onload / onsubmit event handling
    Co-authored-by: Ruben Giosa <rubenalexgiosa@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,40 @@
-window.onload = function() {
-  let form = document.querySelector("form");
-  form.onsubmit = function(event) {
-    event.preventDefault();
+function getInput() {
+  honestInput = document.getElementById("honestInput").value.toLowerCase();
+  heightInput = document.getElementById("heightInput").value.toLowerCase();
+  ageInput = document.getElementById("ageInput").value.toLowerCase();
+};
 
-    const honestInput = document.getElementById("honestInput").value.toLowerCase();
-    const heightInput = document.getElementById("heightInput").value.toLowerCase();
-    const ageInput = document.getElementById("ageInput").value.toLowerCase();
-    
-
-    // Ensure user has input values for every question
-    if (!honestInput || !heightInput || !ageInput) {
-      // Display error message
-      document.getElementById("errorMessage").removeAttribute("class");
+function displayMatch() {
+  // Ensure user has input values for every question
+  if (!honestInput || !heightInput || !ageInput) {
+    // Display error message
+    console.log("Error Print")
+    document.getElementById("errorMessage").removeAttribute("class");
+  } else {
+    // Abe is honest & tall
+    if (honestInput === "important" || heightInput === "important" && ageInput === "not important") {
+      // Display Abe
+      document.getElementById("lincoln").removeAttribute("class");
+    } else if (ageInput === "important") {
+      // Display JFK
+      document.getElementById("kennedy").removeAttribute("class");
     } else {
-      // Abe is honest & tall
-      if (honestInput === "important" || heightInput === "important" && ageInput === "not important") {
-        // Display Abe
-        document.getElementById("lincoln").removeAttribute("class");
-      } else if (ageInput === "important") {
-        // Display JFK
-        document.getElementById("kennedy").removeAttribute("class");
-      } else {
-        document.getElementById("bush").removeAttribute("class");
-      }
+      document.getElementById("bush").removeAttribute("class");
     }
+  };
+};
 
-  }
-}
+window.addEventListener("load", function() {
+  
+  let honestInput;
+  let heightInput;
+  let ageInput;
+
+  let form = document.querySelector("form");
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+  });
+
+  form.addEventListener("submit", getInput);  
+  form.addEventListener("submit", displayMatch);
+});
